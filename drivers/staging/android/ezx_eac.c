e196323906ca224724f52ded60bfa4b1b74c278b
Richard Gráčik - Morc <r.gracik@gmail.com>
2026-08-20 23:21:30 +0200

    ezx_eac: add earpiece/speakerphone switch

--- drivers/staging/android/ezx_eac.c
+++ drivers/staging/android/ezx_eac.c
@@ -3,7 +3,7 @@
    abuses /dev/dsp and /dev/controlC0 to make audio work
    on the MING
 
-   Copyright Richard Gracik @ 370network (mailto:morc@370.network)
+   Copyright (C) 2026 Richard Gracik @ 370network (mailto:morc@370.network)
 
    Based on
 ** drivers/misc/goldfish_audio.c
@@ -33,6 +33,15 @@ MODULE_AUTHOR("Google, Inc. & 370network");
 MODULE_DESCRIPTION("Android EZX Audio Driver");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("1.0");
+
+//AudioHardwareHTC known ioctl names
+#define IOCTL_BT_SCO_AUDIO_PATH_CONTROL 300
+#define IOCTL_SET_SPEAKERPHONE          311
+
+struct eac_audio_files {
+    struct file *ctl_file;
+    struct file *dsp_file;
+};
 
 static long custom_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
@@ -69,26 +78,29 @@ static void control(struct file *ctl, const char *name, int val)
 
 static int eac_audio_open(struct inode *inode, struct file *file)
 {
-	struct file *ctl_file;
-	struct file *dsp_file;
+	struct eac_audio_files *files;
 
-	ctl_file = filp_open("/dev/controlC0", O_RDWR, 0);
-	if (!IS_ERR(ctl_file)) {
-		control(ctl_file, "Master Playback Volume", 10); //volume 0-15 (13 distorts a lot already)
-		control(ctl_file, "Output Mixer AL Switch", 1); //left headphone channel
-		control(ctl_file, "Output Mixer AR Switch", 1); //right headphone channel
-		control(ctl_file, "Output Mixer A1 Switch", 0); //earpiece output
-		control(ctl_file, "Output Mixer A2 Switch", 1); //loudspeaker output
-		control(ctl_file, "Downmixer", 3);	//2->1ch -6db - downmixing for the loudspeaker
+	files = kzalloc(sizeof(*files), GFP_KERNEL);
+	if (!files)
+		return -ENOMEM;
+
+	files->ctl_file = filp_open("/dev/controlC0", O_RDWR, 0);
+	if (!IS_ERR(files->ctl_file)) {
+		control(files->ctl_file, "Master Playback Volume", 10); //volume 0-15 (13 distorts a lot already)
+		control(files->ctl_file, "Output Mixer AL Switch", 1); //left headphone channel
+		control(files->ctl_file, "Output Mixer AR Switch", 1); //right headphone channel
+		control(files->ctl_file, "Output Mixer A1 Switch", 0); //earpiece output
+		control(files->ctl_file, "Output Mixer A2 Switch", 1); //loudspeaker output
+		control(files->ctl_file, "Downmixer", 3);	//2->1ch -6db - downmixing for the loudspeaker
 							//maybe regular 2->1ch is enough, needd more testing
 							//plus disable it for headphones, otherwise you turn mono
-		filp_close(ctl_file, NULL);
+		//filp_close(files->ctl_file, NULL);
 	}
 
-	dsp_file = filp_open("/dev/dsp", O_WRONLY, 0);
-	if (IS_ERR(dsp_file)) {
+	files->dsp_file = filp_open("/dev/dsp", O_WRONLY, 0);
+	if (IS_ERR(files->dsp_file)) {
 		pr_err("eac_audio failed opening /dev/dsp\n");
-		return PTR_ERR(dsp_file);
+		return PTR_ERR(files->dsp_file);
 	}
 
 
@@ -96,22 +108,22 @@ static int eac_audio_open(struct inode *inode, struct file *file)
 	int chan = 2;
 	int bits = 44100;
 
-	custom_ioctl(dsp_file, SNDCTL_DSP_SETFMT, (unsigned long)&fmt);
-	custom_ioctl(dsp_file, SNDCTL_DSP_CHANNELS, (unsigned long)&chan);
-	custom_ioctl(dsp_file, SNDCTL_DSP_SPEED, (unsigned long)&bits);
+	custom_ioctl(files->dsp_file, SNDCTL_DSP_SETFMT, (unsigned long)&fmt);
+	custom_ioctl(files->dsp_file, SNDCTL_DSP_CHANNELS, (unsigned long)&chan);
+	custom_ioctl(files->dsp_file, SNDCTL_DSP_SPEED, (unsigned long)&bits);
 
-	file->private_data = dsp_file;
+	file->private_data = files;
 	return 0;
 }
 
 static ssize_t eac_audio_write(struct file *file, const char __user *buf, size_t count, loff_t *pos)
 {
-	struct file *dsp_file = file->private_data;
+	struct eac_audio_files *files = file->private_data;
 
-	if (!dsp_file || !dsp_file->f_op->write)
+	if (!files->dsp_file || !files->dsp_file->f_op->write)
 		return -ENODEV;
 
-	return vfs_write(dsp_file, buf, count, &dsp_file->f_pos);
+	return vfs_write(files->dsp_file, buf, count, &files->dsp_file->f_pos);
 }
 
 static ssize_t eac_audio_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
@@ -122,24 +134,37 @@ static ssize_t eac_audio_read(struct file *file, char __user *buf, size_t count,
 
 static int eac_audio_release(struct inode *inode, struct file *file)
 {
-	struct file *dsp_file = file->private_data;
+	struct eac_audio_files *files = file->private_data;
 
-	if (dsp_file)
-		filp_close(dsp_file, NULL);
+	if (files->dsp_file)
+		filp_close(files->dsp_file, NULL);
 
 	return 0;
 }
 
 static long eac_audio_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
-	//ignore ioctls flowing into /dev/eac for now
-	//AudioHardwareHTC uses them to switch modes
+	struct eac_audio_files *files = file->private_data;
 
-	/* temporary workaround, until we switch to the ALSA API */
-	if (cmd == 315)
-		return -1;
-	else
-		return 0;
+	printk("eac_audio ioctl cmd %d\n", cmd);
+
+	int value;
+	if (get_user(value, (int __user *)arg)) {
+		return -EFAULT;
+	}
+
+	switch (cmd) {
+		case IOCTL_BT_SCO_AUDIO_PATH_CONTROL: //AudioHardwareHTC::enableBluetooth
+		case 307: //AudioHardwareHTC::setVoiceValue
+		case 315: //AudioHardwareHTC::open
+			return -1;
+		case IOCTL_SET_SPEAKERPHONE: //AudioHardwareHTC::enableSpeaker
+			printk("eac_audio set speakerphone %d\n", value);
+			control(files->ctl_file, "Output Mixer A1 Switch", !value); //earpiece output
+			control(files->ctl_file, "Output Mixer A2 Switch", value); //loudspeaker output
+		default:
+			return 0;
+	}
 }
 
 /* file operations for /dev/eac */
